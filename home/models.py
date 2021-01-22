08656b2556b7b34f16c5ef8ec5a670c2ecacfe61
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2021-01-22 22:12:21 +0530

    db creaded

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class AllTask(models.Model):
+    taskName = models.CharField(max_length=10)
+    about = models.CharField(max_length=25)
+    dateTime = models.DateTimeField()
+    subDateTime = models.DateTimeField()
+    status = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.taskName
+    
