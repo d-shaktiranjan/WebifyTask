a91fea1aa1c81faeaa5b6a35636ef59f441e6ce9
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2021-01-23 00:33:15 +0530

    submit data to db

--- home/views.py
+++ home/views.py
@@ -2,9 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 from home.models import AllTask
+from datetime import datetime
 def index(request):
     tasks = AllTask.objects.all()
     dict = {
         'tasks' : tasks,
     }
+    if request.method == "POST":
+        name = request.POST.get('name')
+        about = request.POST.get('about')
+        date = request.POST.get('date')
+        addNew = AllTask(taskName = name, about = about, subDateTime = datetime.now(), status = "Init", dateTime = datetime.now())
+        addNew.save()
     return render(request, 'index.html', dict)
