aabb116ee24d2d4dc780284eb1b00f2cb555241f
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2021-01-23 00:19:01 +0530

    some db changes

--- home/models.py
+++ home/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 # Create your models here.
 class AllTask(models.Model):
-    taskName = models.CharField(max_length=10)
-    about = models.CharField(max_length=25)
+    task_id = models.AutoField
+    taskName = models.CharField(max_length=30)
+    about = models.CharField(max_length=50)
     dateTime = models.DateTimeField()
-    subDateTime = models.DateTimeField()
+    subDateTime = models.DateTimeField(blank=True, null=True)
     status = models.CharField(max_length=10)
 
     def __str__(self):
