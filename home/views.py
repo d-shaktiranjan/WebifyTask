97649a110d8539fd83e61b68b30d98db90f5dfcc
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2021-02-14 00:05:06 +0530

    fix add task issue

--- home/views.py
+++ home/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 # Create your views here.
 from home.models import AllTask
 from datetime import datetime
-from home import mixSlug
+from home.mixSlug import getSlug
 def index(request):
     tasks = AllTask.objects.all()
     dict = {
@@ -13,7 +13,7 @@ def index(request):
         name = request.POST.get('name')
         about = request.POST.get('about')
         date = request.POST.get('date')
-        slug = mixSlug(name, about)
+        slug = getSlug(name, about)
         addNew = AllTask(taskName = name, about = about, subDateTime = datetime.now(), status = "Init", dateTime = datetime.now(), slug = slug)
         addNew.save()
     return render(request, 'index.html', dict)
